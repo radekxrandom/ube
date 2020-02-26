779724bd7fa74b1b596542aebca8668544e12e55
radek_random <jozwa.zawadiaka@gmail.com>
2020-02-26 19:20:27 +0000

    first commit

---
+++ src/Main.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./App.css";
+import Navbar from "./components/Navbar";
+import Frame0 from "./components/Frame0";
+
+const Main = props => (
+  <div>
+    <Navbar />
+    <Frame0 />
+  </div>
+);
+
+export default Main;
---
+++ src/components/Frame0.js
@@ -0,0 +1,22 @@
+import React from "react";
+
+const Frame0 = props => (
+  <>
+    <div class="top">
+      <button class="t but" id="bt0">
+        KONTAKT
+      </button>
+      <span class="divider"></span>
+      <span class="t mid">
+        <div id="0">UBEZPIECZENIE W IRLANDII</div>
+        <div id="1">ZADBAMY O TWOJE FUTRO</div>
+      </span>
+      <span class="divider"></span>
+      <button class="t but" id="bt1">
+        SZYBKA WYCENA
+      </button>
+    </div>
+  </>
+);
+
+export default Frame0;
