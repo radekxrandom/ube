bfd58724798daa204d7de68045e4170bf53cfecc
radek_random <jozwa.zawadiaka@gmail.com>
2020-08-25 23:53:12 +0200

    css fix

--- front/src/components/ArticlePresentational.js
+++ front/src/components/ArticlePresentational.js
@@ -8,29 +8,31 @@ const ArticlePresentational = props => {
 
   return (
     <>
-      <div className={props.lcl} onClick={i => props.goPlaces(-1)}>
-        <div className="iconleft">
-          <ArrowBackIosIcon className="arrowBack" />
-        </div>
-      </div>
-      <div className={props.rcl} onClick={i => props.goPlaces(1)}>
-        <div className="iconright">
-          <ArrowForwardIosIcon className="arrowForward" />
-        </div>
-      </div>
-      <div className="articles" onKeyDown={props.onArrowPress}>
-        <div className="articleHeader">
-          <div className="image">
-            <img src={props.img} className="img" alt="Pach" />
-          </div>
-          <div className="title">
-            <h1 className="titletext">{props.title}</h1>
+      <div className="articleWrapper">
+        <div className={props.lcl} onClick={i => props.goPlaces(-1)}>
+          <div className="iconleft">
+            <ArrowBackIosIcon className="arrowBack" />
           </div>
         </div>
-        <div className="articletext">
-          {Object.entries(props.article).map((article, i) => (
-            <p>{props.article[i]}</p>
-          ))}
+        <div className={props.rcl} onClick={i => props.goPlaces(1)}>
+          <div className="iconright">
+            <ArrowForwardIosIcon className="arrowForward" />
+          </div>
+        </div>
+        <div className="articles" onKeyDown={props.onArrowPress}>
+          <div className="articleHeader">
+            <div className="image">
+              <img src={props.img} className="img" alt="Pach" />
+            </div>
+            <div className="title">
+              <h1 className="titletext">{props.title}</h1>
+            </div>
+          </div>
+          <div className="articletext">
+            {Object.entries(props.article).map((article, i) => (
+              <p>{props.article[i]}</p>
+            ))}
+          </div>
         </div>
       </div>
     </>
